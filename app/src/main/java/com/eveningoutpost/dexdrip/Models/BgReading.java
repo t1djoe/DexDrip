3729ff580b3073ca87e9fd0672bdebf8a260622e
Joe <jgreenewv@yahoo.com>
2015-02-25 12:09:40 -0500

    Merge pull request #71 from tzachi-dar/tzachi9

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/BgReading.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/BgReading.java
@@ -164,7 +164,7 @@ public class BgReading extends Model {
     }
 
     //*******CLASS METHODS***********//
-    public static BgReading create(double raw_data, double filtered_data, Context context) {
+    public static BgReading create(double raw_data, Context context, Long timestamp) {
         BgReading bgReading = new BgReading();
         Sensor sensor = Sensor.currentSensor();
         if (sensor != null) {
@@ -174,7 +174,7 @@ public class BgReading extends Model {
                 bgReading.sensor_uuid = sensor.uuid;
                 bgReading.raw_data = (raw_data / 1000);
                 bgReading.filtered_data = (filtered_data / 1000);
-                bgReading.timestamp = new Date().getTime();
+                bgReading.timestamp = timestamp;
                 bgReading.uuid = UUID.randomUUID().toString();
                 bgReading.time_since_sensor_started = bgReading.timestamp - sensor.started_at;
                 bgReading.synced = false;
@@ -199,7 +199,7 @@ public class BgReading extends Model {
                 bgReading.calibration_uuid = calibration.uuid;
                 bgReading.raw_data = (raw_data/1000);
                 bgReading.filtered_data = (filtered_data / 1000);
-                bgReading.timestamp = new Date().getTime();
+                bgReading.timestamp = timestamp;
                 bgReading.uuid = UUID.randomUUID().toString();
                 bgReading.time_since_sensor_started = bgReading.timestamp - sensor.started_at;
                 bgReading.synced = false;
