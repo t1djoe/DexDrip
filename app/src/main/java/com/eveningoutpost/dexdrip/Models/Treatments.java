3423de38248a74a9b89293f99b71a7083c6b6e71
Joe <jgreenewv@yahoo.com>
2015-02-25 15:39:57 -0500

    Adding Treatments to Home/Upload

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/Treatments.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/Treatments.java
@@ -28,7 +28,6 @@ import java.util.List;
 @Table(name = "Treatments", id = BaseColumns._ID)
 public class Treatments extends Model {
 
-
     @Expose
     @Column(name = "event_type")
     public String event_type;
